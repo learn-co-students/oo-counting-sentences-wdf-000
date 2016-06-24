8ff158806e09be400a4237608f2c8cad326a139d
ozPop <ozziepopovas@gmail.com>
2016-06-24 15:25:42 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.scan(/(\w+[.!?]+)/).length
+    # binding.pry
   end
 end
