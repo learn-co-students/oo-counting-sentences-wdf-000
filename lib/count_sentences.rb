d3694e03d7bab535a22eb5bcae9e8dd86dd737fc
Hanna Redleaf <hredleaf@gmail.com>
2016-06-29 20:37:23 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")? true : false
   end
 
   def question?
-
+    self.end_with?("?")? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!")? true : false
   end
 
   def count_sentences
-
+    number ||= 0
+    number = self.scan(/\b[.!?]/).size
   end
+
 end
