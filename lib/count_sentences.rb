4f4d868a3dfdbd1a9a65fb19dbc406ba5049f98b
Dmitriy Khait <khaitdmitriy@gmail.com>
2016-07-06 12:58:01 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.split("").last == "."
   end
 
   def question?
-
+    self.split("").last == "?"
   end
 
   def exclamation?
-
+    self.split("").last == "!"
   end
 
   def count_sentences
+    a = self.scan(/\.\z/) + self.scan(/\!\z/) + self.scan(/\?\z/)
+    a.count
 
+    x = self.split(" ").map {|elm| elm.scan(/\.\z/) }.flatten.count
+    y = self.split(" ").map {|elm| elm.scan(/\!\z/) }.flatten.count
+    z = self.split(" ").map {|elm| elm.scan(/\?\z/) }.flatten.count
+
+    return x+y+z
   end
 end
