473b2afc2b341641317b5d36b6b2726e282a57b1
hafizrahman1 <hafiz.bd2009@gmail.com>
2016-07-19 14:52:18 -0400

    updated_counting_sentence

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,6 +16,7 @@ class String
   end
 
   def count_sentences
+    #self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
     sentence = self.split(/[.?!]/)
     sentence.delete("")
     sentence.count
